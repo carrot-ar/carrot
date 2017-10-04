03a54a11c7cb5085e2b48e6f018fbaf650e658b7
Conor Landry <clandry94@ufl.edu>
2017-10-03 21:43:46 -0400

    initial router code

---
+++ routes/routes_test.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"testing"
+)
+
+func TestControllerRouteLookup(t *testing.T) {
+	actual := Lookup("place_sphere")
+
+	expected := Route{
+		controller: "Sphere",
+		function: "Place",
+		persist: false,
+	}
+
+	if actual != expected {
+		t.Errorf("Routes do not match: %v != %v", actual, expected)
+	}
+}
+
+func TestStreamControllerRouteLookup(t *testing.T) {
+	actual := Lookup("draw")
+
+	expected := Route{
+		controller: "Drawing",
+		function: "Draw",
+		persist: true,
+	}
+
+	if actual != expected {
+		t.Errorf("Routes do not match: %v != %v", actual, expected)
+	}
+}
