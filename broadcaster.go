20a8321c09894b4f2b07e470db8c51e4919471cb
Conor Landry <clandry94@ufl.edu>
2017-10-26 17:49:15 -0400

    add broadcaster

--- broadcaster.go
+++ broadcaster.go
@@ -1,36 +1,32 @@
 package carrot
 
+type OutboundMessage struct {
+	message []byte
+}
+
+// manage broadcast groups with the broadcaster
 type Broadcaster struct {
 	sessions SessionStore
-	clients *Clients
+	clientPool  Pool
 	//inbound messages from the clients
 	broadcast chan []byte
 }
 
-func NewBroadcaster(clients *Clients) *Broadcaster {
+func NewBroadcaster(pool Pool) *Broadcaster {
 	return &Broadcaster{
 		sessions:  NewDefaultSessionManager(),
 		broadcast: make(chan []byte, broadcastChannelSize),
-		clients: clients,
+		clientPool: pool,
 	}
 }
 
+// use functions are arguments for broadcasting to the correct groups
 func (br *Broadcaster) broadcastAll(message []byte) {
-	//log.Printf("Length of clients: %v\n", len(br.clients))
-	for _, client := range br.clients {
-		if client != nil {
-			if client.Expired() {
-				br.sessions.Delete(client.session.Token)
-				continue
-			} else if !client.Open() {
-				continue
-			}
-
-			client.session.expireTime = refreshExpiryTime()
-
-			client.send <- message
-		}
+	outboundMessage := OutboundMessage{
+		// set the criteria function here
+		message: message,
 	}
+	br.clientPool.Send(&outboundMessage)
 }
 
 func (br *Broadcaster) Run() {
